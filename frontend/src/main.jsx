1a1c777e12e571a5ab89cb1f9faf07080f1a17ad
Barry Yang <barryayang@gmail.com>
2026-03-24 18:41:50 -0400

    test out reseed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { reseedExerciseLibraryMetaOnly } from './seeds/reseed.js'
 import App from './App.jsx'
+
+window.reseedExerciseLibraryMetaOnly = reseedExerciseLibraryMetaOnly
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
