d3405f827b957fb44b81cd53971d4d0de24e4eef
Barry Yang <barryayang@gmail.com>
2026-03-25 00:41:11 -0400

    backfill deload history

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { reseedExerciseLibraryMetaOnly } from './seeds/reseed.js'
+import { backfillDeloadHistory } from './seeds/reseed'
 import App from './App.jsx'
 
 window.reseedExerciseLibraryMetaOnly = reseedExerciseLibraryMetaOnly
+window.backfillDeloadHistory = backfillDeloadHistory
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
